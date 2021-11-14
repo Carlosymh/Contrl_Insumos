cffeb6e4199fb3e47723b3787dc4137528c8b052
Carlos Yovani Muñoz Hernandez <80989652+Yov-her@users.noreply.github.com>
2021-11-13 20:44:20 -0600

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+   $enlace = mysqli_connect("localhost","id17482893_root","&Ry4fCQCI1HD[%aO","id17482893_insumos");
+   if($enlace ->connect_error){
+    die("Fallo :".$enlace ->connect_error);
+   }
+
+ ?>
