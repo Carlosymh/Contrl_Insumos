c6b0bca29e95adf6aaab1fe6609bbd2a32f456af
Carlos Yovani Muñoz Hernandez <80989652+Yov-her@users.noreply.github.com>
2021-11-13 21:01:43 -0600

    Create registro_fcs_salida.php

---
+++ registros/registro_fcs_salida.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require("../conexion.php");
+date_default_timezone_set('America/Mexico_City');
+   $Fulfillment =$_POST["Fulfillment"];
+    $Tarimas_enviadas=$_POST["Tarimas_enviadas"];
+    $Gaylord_Enviados=$_POST["Gaylord_Enviados"];
+    $cajas=$_POST["cajas"];
+    $costales=$_POST["costales"];
+    $centro_de_trabajo_Destino=$_POST["centro_de_trabajo_Destino"];
+    $Service_Center=$_POST["Service_Center"];
+    $FC_Destino=$_POST["FC_Destino"];
+    $Responsable_Registro=$_SESSION['nombre_usuario'];
+    $date = date('Y-m-d H:i:s', time());
+$sql="INSERT INTO `salida_fc` ( Fulfillment, Tarimas_enviadas, Gaylord_Enviados, cajas, costales, centro_de_trabajo_Destino, Service_Center, FC_Destino, Responsable, Fecha_Creación, Fecha_Hora) VALUES ('$Fulfillment','$Tarimas_enviadas','$Gaylord_Enviados','$cajas','$costales','$centro_de_trabajo_Destino','$Service_Center','$FC_Destino','$Responsable_Registro','$date','$date')";
+$resultado=mysqli_query($enlace,$sql);
+header("location:../form.php");
+?>
